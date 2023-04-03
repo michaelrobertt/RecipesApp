701fc63eb0b01d35096262022c4652860ed14149
Michael <michaelro0913@gmail.com>
2023-04-03 16:45:14 -0300

    Lint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from './pages/Login';
 
 function App() {
   return (
-      <Switch>
-        <Route component={ Login } exact path="/" />
-      </Switch>
+    <Switch>
+      <Route component={ Login } exact path="/" />
+    </Switch>
   );
 }
 
