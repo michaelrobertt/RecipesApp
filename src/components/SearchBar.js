e98c7db4835c35b78405465f4da7671653dabe77
Igor Alegria <igor.alegria@gmail.com>
2023-04-04 14:31:36 -0300

    Requisito 10 - Done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,6 +9,28 @@ function SearchBar() {
         data-testid="search-input"
         name="barraDePesquisa"
       />
+      <input
+        type="radio"
+        data-testid="ingredient-search-radio"
+        name="ingrediente"
+      />
+      <input
+        type="radio"
+        data-testid="name-search-radio"
+        name="nome"
+      />
+      <input
+        type="radio"
+        data-testid="first-letter-search-radio"
+        name="primeiraLetra"
+      />
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        name="botaoDePesquisa"
+      >
+        Pesquisar
+      </button>
     </>
   );
 }
