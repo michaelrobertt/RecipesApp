7101251bd502e53ec6b87c6687a334c6ecb23ac9
Igor Alegria <igor.alegria@gmail.com>
2023-04-05 18:13:21 -0300

    Requisito 20 - Done

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -5,6 +5,7 @@ import Footer from '../components/Footer';
 
 function Drinks() {
   const { respostaDaPesquisa, setRespostaDaPesquisa } = useContext(AppContext);
+  const [categoriaBebida, setCategoriaBebida] = useState();
   const [renderizaReceita, setRederizaReceita] = useState();
   const [carregando, setCarregando] = useState(true);
 
@@ -12,9 +13,11 @@ function Drinks() {
     const requisicaoPadrao = async () => {
       const requisicao = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
       const dados = await requisicao.json();
-      return setRespostaDaPesquisa(dados.drinks);
+      setRespostaDaPesquisa(dados.drinks);
     };
+
     requisicaoPadrao();
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -24,9 +27,17 @@ function Drinks() {
         const numeroDeReceitas = 12;
         const receitas = respostaDaPesquisa.slice(0, numeroDeReceitas);
         setRederizaReceita(receitas);
+      };
+      const requisicaoCategoria = async () => {
+        const numeroDeReceitas = 5;
+        const requisicao = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+        const dados = await requisicao.json();
+        const cincoBebidas = dados.drinks.slice(0, numeroDeReceitas);
+        setCategoriaBebida(cincoBebidas);
         setCarregando(false);
       };
       dozeReceitas();
+      requisicaoCategoria();
     }
   }, [respostaDaPesquisa]);
 
@@ -35,17 +46,38 @@ function Drinks() {
       <Header titulo="Drinks" />
       {carregando ? (<p>Carregando...</p>) : (
         renderizaReceita.map((receita, index) => (
-          <div
-            key={ receita.idDrink }
-            data-testid={ `${index}-recipe-card` }
-          >
-            <img
-              src={ receita.strDrinkThumb }
-              alt="Foto da bebida"
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{receita.strDrink}</p>
+          <div key={ receita.idDrink }>
+            <button
+              type="button"
+              className="meals-btnCategory"
+              onClick={ () => { requisicaoPadrao(); } }
+              data-testid="All-category-filter"
+            >
+              All
+            </button>
+            {categoriaBebida.map((ele, index2) => (
+              <div key={ index2 }>
+                <button
+                  type="button"
+                  className="meals-btnCategory"
+                  id={ ele.strCategory }
+                  data-testid={ `${ele.strCategory}-category-filter` }
+                >
+                  { ele.strCategory }
+                </button>
+              </div>
+            ))}
+            <div
+              data-testid={ `${index}-recipe-card` }
+            >
+              <img
+                src={ receita.strDrinkThumb }
+                alt="Foto da bebida"
+                data-testid={ `${index}-card-img` }
+              />
+              <p data-testid={ `${index}-card-name` }>{receita.strDrink}</p>
 
+            </div>
           </div>
         ))
       )}
