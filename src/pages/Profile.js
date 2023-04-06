d816f12c93fcc041fb871a8943bc89c7f679fc18
Michael <michaelro0913@gmail.com>
2023-04-06 17:05:28 -0300

    Requisitos do 57 até o 61 Completos - Faltando apenas os testes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+
+  const funcaoLogout = () => {
+    localStorage.removeItem('user');
+    history.push('/');
+  };
+
+  useEffect(() => {
+    const userSalvo = localStorage.getItem('user');
+    const emailObject = JSON.parse(userSalvo);
+    const emailSalvo = emailObject.email;
+    setEmail(emailSalvo);
+  }, []);
+
   return (
     <div>
       <Header pesquisaOff titulo="Profile" />
+      <h2 data-testid="profile-email">{`${email}`}</h2>
+      <button
+        onClick={ () => history.push('/done-recipes') }
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        onClick={ () => history.push('/favorite-recipes') }
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        onClick={ funcaoLogout }
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
