b43b798600202e7e6b1f6fea65aa9f0dc546c3c0
Igor Alegria <igor.alegria@gmail.com>
2023-04-10 14:47:56 -0300

    Requisito 26 - Done

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import AppContext from '../context/AppContext';
 
 function RecipeDetails({ match: { params: { id } }, location: { pathname } }) {
-  const { respostaDaPesquisa, setRespostaDaPesquisa } = useContext(AppContext);
+  // const { respostaDaPesquisa, setRespostaDaPesquisa } = useContext(AppContext);
+  const [receita, setReceita] = useState();
+  const [recomendacoes, setRecomendacoes] = useState();
   const [ingredientes, setingredientes] = useState(null);
   const [tipo, setTipo] = useState(null);
   const [carregando, setCarregando] = useState(true);
@@ -13,31 +14,37 @@ function RecipeDetails({ match: { params: { id } }, location: { pathname } }) {
       if (pathname.includes('drinks')) {
         const requisicao = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
         const dados = await requisicao.json();
-        setRespostaDaPesquisa(dados);
+        const requisicaoRecomendacoes = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+        const dadosRecomendacoes = await requisicaoRecomendacoes.json();
+        setReceita(dados);
+        setRecomendacoes(dadosRecomendacoes);
       } else {
         const requisicao = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
         const dados = await requisicao.json();
-        setRespostaDaPesquisa(dados);
+        const requisicaoRecomendacoes = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        const dadosRecomendacoes = await requisicaoRecomendacoes.json();
+        setReceita(dados);
+        setRecomendacoes(dadosRecomendacoes);
       }
     };
     requisicaoDeReceita();
   }, []);
 
   useEffect(() => {
-    if (respostaDaPesquisa) {
-      const chaveIngredientes = Object.keys(respostaDaPesquisa).toString();
-      const chave = Object.keys(respostaDaPesquisa[chaveIngredientes][0]);
+    if (receita) {
+      const chaveIngredientes = Object.keys(receita).toString();
+      const chave = Object.keys(receita[chaveIngredientes][0]);
       const todosIngredientes = [];
       chave.forEach((element) => {
         if (element.includes('strIngredient')
-        && respostaDaPesquisa[chaveIngredientes][0][element]) {
+        && receita[chaveIngredientes][0][element]) {
           todosIngredientes.push(element);
         }
         setingredientes(todosIngredientes);
         setTipo(chaveIngredientes);
       });
     }
-  }, [respostaDaPesquisa]);
+  }, [receita]);
 
   useEffect(() => {
     if (ingredientes) {
@@ -51,25 +58,25 @@ function RecipeDetails({ match: { params: { id } }, location: { pathname } }) {
       {!pathname.includes('drinks') ? (
         <section>
           <img
-            src={ respostaDaPesquisa[tipo][0].strMealThumb }
-            alt={ respostaDaPesquisa[tipo][0].strMeal }
+            src={ receita[tipo][0].strMealThumb }
+            alt={ receita[tipo][0].strMeal }
             data-testid="recipe-photo"
           />
-          <h3 data-testid="recipe-title">{respostaDaPesquisa[tipo][0].strMeal}</h3>
-          <p data-testid="recipe-category">{respostaDaPesquisa[tipo][0].strCategory}</p>
+          <h3 data-testid="recipe-title">{receita[tipo][0].strMeal}</h3>
+          <p data-testid="recipe-category">{receita[tipo][0].strCategory}</p>
           {ingredientes.map((elemento, index) => (
             <p key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>
-              {respostaDaPesquisa[tipo][0][elemento]}
+              {receita[tipo][0][elemento]}
               {' '}
-              {respostaDaPesquisa[tipo][0][`strMeasure${index + 1}`]}
+              {receita[tipo][0][`strMeasure${index + 1}`]}
             </p>
           ))}
-          <p data-testid="instructions">{respostaDaPesquisa[tipo][0].strInstructions}</p>
+          <p data-testid="instructions">{receita[tipo][0].strInstructions}</p>
           <iframe
             data-testid="video"
             width="560"
             height="315"
-            src={ respostaDaPesquisa[tipo][0].strYoutube.replace('watch?v=', 'embed/') }
+            src={ receita[tipo][0].strYoutube.replace('watch?v=', 'embed/') }
             title="YouTube video player"
             allow="accelerometer;
                   clipboard-write;
@@ -82,20 +89,20 @@ function RecipeDetails({ match: { params: { id } }, location: { pathname } }) {
       ) : (
         <section>
           <img
-            src={ respostaDaPesquisa[tipo][0].strDrinkThumb }
-            alt={ respostaDaPesquisa[tipo][0].strDrink }
+            src={ receita[tipo][0].strDrinkThumb }
+            alt={ receita[tipo][0].strDrink }
             data-testid="recipe-photo"
           />
-          <h3 data-testid="recipe-title">{respostaDaPesquisa[tipo][0].strDrink}</h3>
-          <p data-testid="recipe-category">{respostaDaPesquisa[tipo][0].strAlcoholic}</p>
+          <h3 data-testid="recipe-title">{receita[tipo][0].strDrink}</h3>
+          <p data-testid="recipe-category">{receita[tipo][0].strAlcoholic}</p>
           {ingredientes.map((elemento, index) => (
             <p key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>
-              {respostaDaPesquisa[tipo][0][elemento]}
+              {receita[tipo][0][elemento]}
               {' '}
-              {respostaDaPesquisa[tipo][0][`strMeasure${index + 1}`]}
+              {receita[tipo][0][`strMeasure${index + 1}`]}
             </p>
           ))}
-          <p data-testid="instructions">{respostaDaPesquisa[tipo][0].strInstructions}</p>
+          <p data-testid="instructions">{receita[tipo][0].strInstructions}</p>
         </section>
       )}
     </div>
