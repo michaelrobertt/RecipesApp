881aee9ff59217d418ea13706175e0da32a4c2d8
Igor Alegria <igor.alegria@gmail.com>
2023-04-11 14:22:19 -0300

    Requisito 33 - Done

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Copy from 'clipboard-copy';
 
 function ShareButton() {
+  const { href } = window.location;
+  const [copiado, setCopiado] = useState(false);
+
+  const copiarLink = () => {
+    Copy(href);
+    setCopiado(true);
+  };
+
   return (
     <div>
       <button
         type="button"
         data-testid="share-btn"
+        onClick={ copiarLink }
       >
         Share
       </button>
+      {copiado && <p>Link copied!</p> }
     </div>
   );
 }
