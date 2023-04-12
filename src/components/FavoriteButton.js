d300485f3214240bf95ac0cc624f5ce019e5bca7
Michael <michaelro0913@gmail.com>
2023-04-12 15:31:26 -0300

    Recipes CSS done

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -21,6 +21,7 @@ function FavoriteButton() {
         setreceitaFavorita(estaFavoritado);
       }
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const formatoReceitaData = () => {
