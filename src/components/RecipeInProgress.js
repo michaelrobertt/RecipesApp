382579fa07aea5a236ea26b1401ac48a9a359673
Michael <michaelro0913@gmail.com>
2023-04-13 14:39:41 -0300

    Pagina recipeInProgress CSS

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 import ShareButton from './ShareButton';
 import FavoriteButton from './FavoriteButton';
@@ -7,6 +8,7 @@ import '../App.css';
 
 function RecipeInProgress({ match: { params: { id } }, location: { pathname } }) {
   const { receita, setReceita } = useContext(AppContext);
+  const history = useHistory();
   const [ingredientes, setingredientes] = useState(null);
   const [tipo, setTipo] = useState(null);
   const [carregando, setCarregando] = useState(true);
@@ -72,95 +74,167 @@ function RecipeInProgress({ match: { params: { id } }, location: { pathname } })
         drinks: { ...receitasEmProgresso.drinks,
           [id]: ingredientesSelecionados } }));
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ingredientesSelecionados]);
 
   if (carregando) { return <h1>Carregando...</h1>; }
   return (
     <div>
       {!pathname.includes('drinks') ? (
-        <section>
-          <img
-            src={ receita[tipo][0].strMealThumb }
-            alt={ receita[tipo][0].strMeal }
-            data-testid="recipe-photo"
-          />
-          <h1 data-testid="recipe-title">{receita[tipo][0].strMeal}</h1>
-          <ShareButton />
-          <FavoriteButton />
-          <p data-testid="recipe-category">{receita[tipo][0].strCategory}</p>
-          <h2>Ingredients</h2>
-          {ingredientes.map((elemento, index) => (
-            <div key={ index }>
-              <label
-                className={ ingredientesSelecionados.includes(receita[tipo][0][elemento])
-                  ? 'strike' : '' }
-                htmlFor={ receita.meals.idMeal }
-                key={ index }
-                data-testid={ `${index}-ingredient-step` }
-              >
-                <input
-                  type="checkbox"
-                  id={ receita.meals.idMeal }
-                  name={ receita[tipo][0][elemento] }
-                  value={ receita[tipo][0][elemento] }
-                  checked={ ingredientesSelecionados
-                    .includes(receita[tipo][0][elemento]) }
-                  onChange={ handleCheckboxChange }
-                />
-                {receita[tipo][0][elemento]}
-                {' '}
-                {receita[tipo][0][`strMeasure${index + 1}`]}
-              </label>
+        <section className="recipeMealDrink">
+          <div className="recipe-btn">
+            <button
+              className="recipe-btns"
+              onClick={ () => history.push('/meals') }
+            >
+              Voltar
+            </button>
+            <div className="interact-btn">
+              <FavoriteButton />
+              <ShareButton />
             </div>
-          ))}
-          <h2>Instructions</h2>
-          <p data-testid="instructions">{receita[tipo][0].strInstructions}</p>
+          </div>
+          <div className="recipeImage">
+            <img
+              src={ receita[tipo][0].strMealThumb }
+              alt={ receita[tipo][0].strMeal }
+              data-testid="recipe-photo"
+            />
+          </div>
+          <div className="recipedetail">
+            <div className="recipe-info">
+              <h1
+                className="recipe-info-title"
+                data-testid="recipe-title"
+              >
+                {receita[tipo][0].strMeal}
+
+              </h1>
+              <p
+                className="recipe-info-title"
+                data-testid="recipe-category"
+              >
+                {receita[tipo][0].strCategory}
+              </p>
+              <h2>Ingredients</h2>
+              {ingredientes.map((elemento, index) => (
+                <div key={ index }>
+                  <label
+                    className={ ingredientesSelecionados
+                      .includes(receita[tipo][0][elemento])
+                      ? 'strike' : '' }
+                    htmlFor={ receita.meals.idMeal }
+                    key={ index }
+                    data-testid={ `${index}-ingredient-step` }
+                  >
+                    <input
+                      type="checkbox"
+                      id={ receita.meals.idMeal }
+                      name={ receita[tipo][0][elemento] }
+                      value={ receita[tipo][0][elemento] }
+                      checked={ ingredientesSelecionados
+                        .includes(receita[tipo][0][elemento]) }
+                      onChange={ handleCheckboxChange }
+                    />
+                    {receita[tipo][0][elemento]}
+                    {' '}
+                    {receita[tipo][0][`strMeasure${index + 1}`]}
+                  </label>
+                </div>
+              ))}
+            </div>
+            <h2>Instructions</h2>
+            <p
+              className="recipeP"
+              data-testid="instructions"
+            >
+              {receita[tipo][0].strInstructions}
+            </p>
+            <div className="tutorial">
+              <h4>Tutorial</h4>
+              <iframe
+                data-testid="video"
+                className="video"
+                src={ receita[tipo][0].strYoutube.replace('watch?v=', 'embed/') }
+                title="YouTube video player"
+                allow="accelerometer;
+              clipboard-write;
+              encrypted-media;
+              gyroscope;
+              picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </div>
         </section>
       ) : (
-        <section>
-          <img
-            src={ receita[tipo][0].strDrinkThumb }
-            alt={ receita[tipo][0].strDrink }
-            data-testid="recipe-photo"
-          />
-          <h1 data-testid="recipe-title">{receita[tipo][0].strDrink}</h1>
-          <ShareButton />
-          <FavoriteButton />
-          <p data-testid="recipe-category">{receita[tipo][0].strAlcoholic}</p>
-          <h2>Ingredients</h2>
-          {ingredientes.map((elemento, index) => (
-            <div key={ index }>
-              <label
-                className={ ingredientesSelecionados.includes(receita[tipo][0][elemento])
-                  ? 'strike' : '' }
-                key={ index }
-                data-testid={ `${index}-ingredient-step` }
-                htmlFor={ receita.drinks.idDrinks }
-              >
-                <input
-                  type="checkbox"
-                  id={ receita.drinks.idDrinks }
-                  name={ receita[tipo][0][elemento] }
-                  value={ receita[tipo][0][elemento] }
-                  checked={ ingredientesSelecionados
-                    .includes(receita[tipo][0][elemento]) }
-                  onChange={ handleCheckboxChange }
-                />
-                {receita[tipo][0][elemento]}
-                {' '}
-                {receita[tipo][0][`strMeasure${index + 1}`]}
-              </label>
+        <section className="recipeMealDrink">
+          <div className="recipe-btn">
+            <button
+              className="recipe-btns"
+              onClick={ () => history.push('/meals') }
+            >
+              Voltar
+            </button>
+            <div className="interact-btn">
+              <FavoriteButton />
+              <ShareButton />
             </div>
-          ))}
-          <h2>Instructions</h2>
-          <p data-testid="instructions">{receita[tipo][0].strInstructions}</p>
+          </div>
+          <div className="recipeImage">
+            <img
+              src={ receita[tipo][0].strDrinkThumb }
+              alt={ receita[tipo][0].strDrink }
+              data-testid="recipe-photo"
+            />
+          </div>
+          <div className="recipedetail">
+            <div className="recipe-info">
+              <h1 data-testid="recipe-title">{receita[tipo][0].strDrink}</h1>
+              <p data-testid="recipe-category">{receita[tipo][0].strAlcoholic}</p>
+              <h2>Ingredients</h2>
+              {ingredientes.map((elemento, index) => (
+                <div key={ index }>
+                  <label
+                    className={ ingredientesSelecionados
+                      .includes(receita[tipo][0][elemento])
+                      ? 'strike' : '' }
+                    key={ index }
+                    data-testid={ `${index}-ingredient-step` }
+                    htmlFor={ receita.drinks.idDrinks }
+                  >
+                    <input
+                      type="checkbox"
+                      id={ receita.drinks.idDrinks }
+                      name={ receita[tipo][0][elemento] }
+                      value={ receita[tipo][0][elemento] }
+                      checked={ ingredientesSelecionados
+                        .includes(receita[tipo][0][elemento]) }
+                      onChange={ handleCheckboxChange }
+                    />
+                    {receita[tipo][0][elemento]}
+                    {' '}
+                    {receita[tipo][0][`strMeasure${index + 1}`]}
+                  </label>
+                </div>
+              ))}
+            </div>
+            <h2>Instructions</h2>
+            <p data-testid="instructions">{receita[tipo][0].strInstructions}</p>
+          </div>
         </section>
       )}
-      <button data-testid="finish-recipe-btn">Finalizar Receita</button>
+      <div className="buttonRecipe">
+        <button
+          className="receitaButton"
+          data-testid="finish-recipe-btn"
+        >
+          Finalizar Receita
+        </button>
+      </div>
     </div>
   );
 }
-
 RecipeInProgress.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string,
@@ -171,5 +245,4 @@ RecipeInProgress.propTypes = {
     }),
   }),
 }.isRequired;
-
 export default RecipeInProgress;
